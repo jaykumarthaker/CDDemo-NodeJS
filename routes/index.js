8fc86ea778740c804891e6803c387143b3605615
Jay Thaker <jay@canaray.com>
2019-05-27 18:33:46 -0400

    updated to 5

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express V4.1' });
+  res.render('index', { title: 'Express V5' });
 });
 
 module.exports = router;
